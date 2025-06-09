59a5c77b40c68d158cbf1ad47f45483817e83ddf
Uth <46614539+youthtion@users.noreply.github.com>
2025-06-09 08:40:45 +0800

    Add files via upload

---
+++ puzzle/assets/Scripts/BuffController.ts
@@ -0,0 +1,272 @@
+import { _decorator, Component, Node, Label, CCInteger, Game } from 'cc';
+import { MIN_BOARD_LENGTH, MAX_BOARD_LENGTH, ADD_NEAR_RATE, GameModel } from './GameModel'
+const { ccclass, property } = _decorator;
+
+const MAX_SIZE_BUFF = 3;
+const MIN_SIZE_BUFF = -3;
+const MAX_OBSTACLE_BUFF = 4;
+const MAX_ONECELL_BUFF = 5;
+const MAX_BUFF_NUM = 3;
+
+enum EBuffType{
+    BT_GROW_STOP,
+    BT_GROW_DEC,
+    BT_SMALLER_PUZZLE,
+    BT_BIGGER_PUZZLE,
+    BT_LESS_HUE,
+    BT_MORE_HUE,
+    BT_ADD_OBSTACLE,
+    BT_DEC_OBSTACLE,
+    BT_GET_1X1_PUZZLE,
+    BT_LOSE_1X1_PUZZLE,
+    BT_CANNOT_ROTATE,
+    BT_LESS_BUFF,
+    BT_MORE_BUFF,
+    BT_REFRESH,
+    BT_GRAVITY,
+    BT_BUOYANCY,
+    BT_PASS,
+}
+
+@ccclass('BuffController')
+export class BuffController extends Component {
+
+    @property({type:Node})
+    public refreshBtn:Node|null = null;
+    @property({type:Label})
+    public freshLabel:Label|null = null;
+    @property({type:Node})
+    public buffBtn1:Node|null = null;
+    @property({type:Node})
+    public buffBtn2:Node|null = null;
+    @property({type:Node})
+    public buffBtn3:Node|null = null;
+    @property({type:Label})
+    public buffLabel1:Label|null = null;
+    @property({type:Label})
+    public buffLabel2:Label|null = null;
+    @property({type:Label})
+    public buffLabel3:Label|null = null;
+    @property({type:CCInteger})
+    public buffSpace:number = 0;
+
+    start()
+    {
+        if(this.buffBtn1){
+            this.buffBtns.push(this.buffBtn1);
+        }
+        if(this.buffBtn2){
+            this.buffBtns.push(this.buffBtn2);
+        }
+        if(this.buffBtn3){
+            this.buffBtns.push(this.buffBtn3);
+        }
+        if(this.buffLabel1){
+            this.buffLabels.push(this.buffLabel1);
+        }
+        if(this.buffLabel2){
+            this.buffLabels.push(this.buffLabel2);
+        }
+        if(this.buffLabel3){
+            this.buffLabels.push(this.buffLabel3);
+        }
+    }
+
+    update(deltaTime: number) {}
+
+    showBuffBtn()
+    {
+        for(let i = 0; i < this.buffBtns.length; i++){
+            if(this.buffNum > i){
+                this.buffBtns[i].active = true;
+                this.buffBtns[i].setPosition(this.buffSpace * (this.buffNum - 1) * (-0.5) + i * this.buffSpace, 0, 0);
+                this.buffLabels[i].string = this.buffText[this.buffList[i]];
+            }
+            else{
+                this.buffBtns[i].active = false;
+            }
+        }
+        if(this.refreshBtn){
+            this.refreshBtn.active = this.refreshNum > 0;
+            if(this.freshLabel){
+                this.freshLabel.string = '' + this.refreshNum;
+            }
+        }
+    }
+
+    onBuffClick(_e:Event, _id:number)
+    {
+        this.setBuff(_id);
+    }
+
+    onRefreshClick()
+    {
+        this.refreshNum -= 1;
+        this.generateBuffList();
+        this.showBuffBtn();
+    }
+
+    getBuffAddNearRate():number[]
+    {
+        let add_near_rate = ADD_NEAR_RATE.slice();
+        for(let i = 3; i < add_near_rate.length; i++){
+            add_near_rate[i] += GameModel.getPuzzleSizeBuff() * 0.1;
+            if(add_near_rate[i] > 1.0){
+                add_near_rate[i] = 1.0;
+            }
+            else if(add_near_rate[i] < 0){
+                add_near_rate[i] = 0;
+            }
+        }
+        return add_near_rate;
+    }
+
+    setBuff(_id:number)
+    {
+        let type:EBuffType = this.buffList[_id];
+        switch(type){
+            case EBuffType.BT_GROW_STOP:
+                break;
+            case EBuffType.BT_GROW_DEC:
+                if(GameModel.getBoardLength() > MIN_BOARD_LENGTH){
+                    GameModel.setBoardLength(GameModel.getBoardLength() - 1);
+                }
+                break;
+            case EBuffType.BT_SMALLER_PUZZLE:
+                GameModel.setPuzzleSizeBuff(GameModel.getPuzzleSizeBuff() - 1);
+                break;
+            case EBuffType.BT_BIGGER_PUZZLE:
+                GameModel.setPuzzleSizeBuff(GameModel.getPuzzleSizeBuff() + 1);
+                break;
+            case EBuffType.BT_LESS_HUE:
+                GameModel.setLessHueBuff(GameModel.getLessHueBuff() + 1);
+                break;
+            case EBuffType.BT_MORE_HUE:
+                GameModel.setLessHueBuff(GameModel.getLessHueBuff() - 1);
+                break;
+            case EBuffType.BT_ADD_OBSTACLE:
+                GameModel.setObstacleBuff(GameModel.getObstacleBuff() + 1);
+                break;
+            case EBuffType.BT_DEC_OBSTACLE:
+                GameModel.setObstacleBuff(GameModel.getObstacleBuff() - 1);
+                break;
+            case EBuffType.BT_GET_1X1_PUZZLE:
+                GameModel.setOnecellBuff(GameModel.getOnecellBuff() + 1);
+                break;
+            case EBuffType.BT_LOSE_1X1_PUZZLE:
+                GameModel.setOnecellBuff(GameModel.getOnecellBuff() - 1);
+                this.buffList[_id] = EBuffType.BT_GROW_DEC;
+                this.showBuffBtn();
+                return;
+            case EBuffType.BT_CANNOT_ROTATE:
+                GameModel.setCannotRotateBuff(true);
+                this.buffList[_id] = EBuffType.BT_GROW_DEC;
+                this.showBuffBtn();
+                return;
+            case EBuffType.BT_LESS_BUFF:
+                this.buffNum -= 1;
+                this.refreshNum += 1;
+                this.buffList.splice(_id, 1);
+                this.showBuffBtn();
+                return;
+            case EBuffType.BT_MORE_BUFF:
+                this.buffNum += 1;
+                [this.buffList[_id], this.buffList[this.buffList.length - 1]] = [this.buffList[this.buffList.length - 1], this.buffList[_id]]
+                this.showBuffBtn();
+                return;
+            case EBuffType.BT_REFRESH:
+                this.generateBuffList();
+                this.showBuffBtn();
+                return;
+            case EBuffType.BT_GRAVITY:
+                GameModel.setGravityBuff(1);
+                break;
+            case EBuffType.BT_BUOYANCY:
+                GameModel.setGravityBuff(-1);
+                break;
+            case EBuffType.BT_PASS:
+                break;
+        }
+        if(type > EBuffType.BT_GROW_DEC && GameModel.getBoardLength() < MAX_BOARD_LENGTH){
+            GameModel.setBoardLength(GameModel.getBoardLength() + 1);
+        }
+        this.node.emit('onSetBuffFinish');
+    }
+
+    generateBuffList()
+    {
+        this.buffList = [];
+        if(GameModel.getBoardLength() < MAX_BOARD_LENGTH){
+            this.buffList.push(EBuffType.BT_GROW_STOP);
+        }
+        if(GameModel.getPuzzleSizeBuff() > MIN_SIZE_BUFF){
+            this.buffList.push(EBuffType.BT_SMALLER_PUZZLE);
+        }
+        if(GameModel.getPuzzleSizeBuff() < MAX_SIZE_BUFF){
+            this.buffList.push(EBuffType.BT_BIGGER_PUZZLE);
+        }
+        this.buffList.push(EBuffType.BT_LESS_HUE);
+        if(GameModel.getLessHueBuff() > 1){
+            this.buffList.push(EBuffType.BT_MORE_HUE);
+        }
+        if(GameModel.getObstacleBuff() < MAX_OBSTACLE_BUFF){
+            this.buffList.push(EBuffType.BT_ADD_OBSTACLE);
+        }
+        if(GameModel.getObstacleBuff() > 0){
+            this.buffList.push(EBuffType.BT_DEC_OBSTACLE);
+        }
+        if(GameModel.getOnecellBuff() < MAX_ONECELL_BUFF){
+            this.buffList.push(EBuffType.BT_GET_1X1_PUZZLE);
+        }
+        if(GameModel.getBoardLength() > MIN_BOARD_LENGTH){
+            if(GameModel.getOnecellBuff() > 0){
+                this.buffList.push(EBuffType.BT_LOSE_1X1_PUZZLE);
+            }
+            if(!GameModel.getCannotRotateBuff()){
+                this.buffList.push(EBuffType.BT_CANNOT_ROTATE);
+            }
+        }
+        if(this.buffNum > 1){
+            this.buffList.push(EBuffType.BT_LESS_BUFF);
+        }
+        if(this.buffNum < MAX_BUFF_NUM){
+            this.buffList.push(EBuffType.BT_MORE_BUFF);
+        }
+        this.buffList.push(EBuffType.BT_REFRESH);
+        if(GameModel.getGravityBuff() == 0){
+            this.buffList.push(EBuffType.BT_GRAVITY);
+            this.buffList.push(EBuffType.BT_BUOYANCY);
+        }
+        this.buffList.push(EBuffType.BT_PASS);
+        for(let i = 0; i < this.buffList.length; i++){
+            let rand = Math.floor(Math.random() * (this.buffList.length - i)) + i;
+            [this.buffList[i], this.buffList[rand]] = [this.buffList[rand], this.buffList[i]];
+        }
+    }
+
+    private buffBtns:Node[] = [];
+    private buffLabels:Label[] = [];
+    private buffList:EBuffType[] = [];
+    private buffNum:number = 2;
+    private refreshNum:number = 0;
+    private buffText:string[] = [
+        '底板不擴大(1場)',
+        '底板縮小',
+        '小方塊變多',
+        '大方塊變多',
+        '方塊色差小',
+        '方塊色差大',
+        '底板缺格增加',
+        '底板缺格減少',
+        '獲得1x1方塊',
+        '失去1x1方塊\n出現底板縮小',
+        '不可旋轉(1場)\n出現底板縮小',
+        '選項減少\n獲得刷新次數',
+        '選項增加',
+        '刷新選項',
+        '產生重力(1場)',
+        '產生浮力(1場)',
+        '跳過'];
+}
+
+
---
+++ puzzle/assets/Scripts/GameController.ts
@@ -0,0 +1,456 @@
+import { _decorator, Component, Node, Prefab, instantiate, Sprite, Label, EditBox, game } from 'cc';
+import { MAX_BOARD_LENGTH, MIN_BOARD_LENGTH, CELL_WIDTH, BOARD_COLOR, OBSTACLE_COLOR, ADD_NEAR_RATE, GameModel } from './GameModel'
+import { PuzzleController } from './PuzzleController'
+import { BuffController } from './BuffController'
+const { ccclass, property } = _decorator;
+
+@ccclass('GameController')
+export class GameController extends Component {
+
+    @property({type:Prefab})
+    private cellPrefab:Prefab|null = null;
+    @property({type:PuzzleController})
+    private puzzleCtrl:PuzzleController|null = null;
+    @property({type:BuffController})
+    private buffCtrl:BuffController|null = null;
+    @property({type:Node})
+    public startMenu:Node|null = null;
+    @property({type:Node})
+    public btnStart:Node|null = null;
+    @property({type:Label})
+    public titleLabel:Label|null = null;
+    @property({type:Label})
+    public stageLabel:Label|null = null;
+    @property({type:Node})
+    public shareBtn:Node|null = null;
+    @property({type:Node})
+    public shareMenu:Node|null = null;
+    @property({type:EditBox})
+    public shareEdit:EditBox|null = null;
+    @property({type:Node})
+    public screenshotBtn:Node|null = null;
+
+    start()
+    {
+        this.checkHelpMode();
+        this.buffCtrl?.node.on('onSetBuffFinish', this.onStartButtonClicked, this);
+    }
+
+    //初始化單局資訊
+    restart()
+    {
+        this.setMenuVisible(false); //遊戲進行, 隱藏選單
+        //更新棋盤大小並初始化單局資訊
+        if(this.stageLabel){
+            this.stageLabel.string = 'Stage' + GameModel.getStage();
+        }
+        this.initBoard();
+        this.generatePuzzle();
+        this.puzzleCtrl?.initPuzzle();
+        this.puzzleCtrl?.setEventsActive(true);
+        this.setEventsActive(true);
+    }
+
+    pause()
+    {
+        //場間階段, 叫出選單選擇BUFF
+        this.setMenuVisible(true);
+        if(this.btnStart){
+            this.btnStart.active = false;
+        }
+        if(this.titleLabel){
+            this.titleLabel.string = '';
+        }
+        if(this.stageLabel){
+            this.stageLabel.string = '';
+        }
+        GameModel.setStage(GameModel.getStage() + 1);
+        GameModel.setCannotRotateBuff(false);
+        GameModel.setGravityBuff(0);
+        this.buffCtrl?.generateBuffList();
+        this.buffCtrl?.showBuffBtn();
+        this.puzzleCtrl?.setEventsActive(false);
+        this.setEventsActive(false);
+    }
+
+    update(deltaTime: number){}
+
+    setEventsActive(_set:boolean)
+    {
+        if(_set){
+            this.puzzleCtrl?.node.on('onFitPuzzle', this.onFitPuzzle, this); //接收拼圖放上棋盤事件
+            this.puzzleCtrl?.node.on('onRemovePuzzle', this.onRemovePuzzle, this); //接收拼圖移出棋盤事件
+        }
+        else{
+            this.puzzleCtrl?.node.off('onFitPuzzle', this.onFitPuzzle, this);
+            this.puzzleCtrl?.node.off('onRemovePuzzle', this.onRemovePuzzle, this);
+        }
+    }
+
+    //初始化棋盤資訊
+    initBoard(_obstacle_set:Set<number> = new Set<number>())
+    {
+        if(!this.cellPrefab){
+            return;
+        }
+        let board_length = GameModel.getBoardLength(); //棋盤大小
+        let board:number[][] = []; //空棋盤資訊
+        this.node.removeAllChildren(); //清除棋盤節點
+        if(_obstacle_set.size == 0 && GameModel.getObstacleBuff() > 0){
+            let obstacle_buff = GameModel.getObstacleBuff();
+            let part_len = Math.floor(board_length * board_length / obstacle_buff);
+            for(let i = 0; i < obstacle_buff; i++){
+                _obstacle_set.add(Math.floor(Math.random() * part_len) + part_len * i);
+            }
+        }
+        for(let i = 0; i < board_length; i++){
+            let line = [];
+            for(let j = 0; j < board_length; j++){
+                //生成ixj個cell加入棋盤節點
+                let cell:Node|null = instantiate(this.cellPrefab);
+                if(cell){
+                    this.node.addChild(cell);
+                    cell.setPosition(i * CELL_WIDTH, j * CELL_WIDTH, 0);
+                    let cell_sprite = cell.getComponent(Sprite);
+                    if(_obstacle_set.size > 0 && _obstacle_set.has(i * board_length + j)){
+                        if(cell_sprite){
+                            cell_sprite.color = OBSTACLE_COLOR;
+                        }
+                        line.push(Infinity);
+                    }
+                    else{
+                        if(cell_sprite){
+                            cell_sprite.color = BOARD_COLOR;
+                        }
+                        line.push(-1); //初始棋盤資訊每格填入-1
+                    }
+                }
+            }
+            board.push(line);
+        }
+        GameModel.setBoard(board); //更新棋盤資訊給GameModel
+        //棋盤節點置中於畫面
+        let pos = board_length * CELL_WIDTH * (-0.5) + CELL_WIDTH * (0.5);
+        this.node.setPosition(pos, pos, 0);
+    }
+
+    //生成拼圖
+    generatePuzzle()
+    {
+        let board:number[][] = GameModel.getBoard().map(v => v.slice());
+        let puzzles:number[][][] = []; //空拼圖資訊
+        let onecell_buff = GameModel.getOnecellBuff();
+        for(let i = 0; i < board.length; i++){
+            for(let j = 0; j < board[i].length; j++){
+                //遍歷棋盤資訊尋找初始格-1
+                if(board[i][j] == -1){
+                    let puzzle_id = puzzles.length;
+                    //將i,j格設定為拼圖第一個CELL
+                    puzzles.push([[i, j]]);
+                    if(onecell_buff > 0){
+                        board[i][j] = Infinity;
+                        onecell_buff--;
+                    }
+                    else{
+                        board[i][j] = puzzle_id;
+                        this.addNearCell(puzzles, board); //遞迴生成鄰近CELL
+                        //生成結果若只有1CELL, 加進鄰近拼圖
+                        if(puzzles[puzzle_id].length <= 1){
+                            let [x, y] = puzzles[puzzle_id][0];
+                            let near_puzzle_id = this.findNearPuzzle(board, x, y);
+                            puzzles[near_puzzle_id].push(puzzles[puzzle_id][0]);
+                            puzzles.pop();
+                            board[x][y] = near_puzzle_id;
+                        }
+                    }
+                }
+            }
+        }
+        GameModel.setPuzzles(puzzles); //更新拼圖資訊給GameModel
+    }
+
+    //遞迴生成鄰近CELL
+    addNearCell(_puzzles:number[][][], _board:number[][])
+    {
+        let puzzle_id = _puzzles.length - 1;
+        let puzzle = _puzzles[puzzle_id];
+        let board:number[][] = _board.map(v => v.slice()); //複製棋盤資訊暫時修改
+        let new_cell:number[][] = []; //候選生成CELL
+        let direct = [[1, 0], [0, 1]]; //往右下尋找可生成CELL(避免太容易壓縮其他拼圖生成空間不往左上生成)
+        //遍歷現有CELL
+        for(let i = 0; i < puzzle.length; i++){
+            let [cx, cy] = puzzle[i]; //CELL的x,y
+            //搜尋右與下棋盤
+            for(let j = 0; j < direct.length; j++){
+                let [dx, dy] = direct[j];
+                let [x, y] = [cx + dx, cy + dy]; //右與下CELL的x,y
+                //若在棋盤範圍內且未被占用(-1)則加入候選
+                if(x < board.length && y < board[x].length && board[x][y] == -1){
+                    new_cell.push([x, y]);
+                    board[x][y] = 0; //選入CELL後棋盤上設定為佔用
+                }
+            }
+        }
+        //若有候選且CELL數在機率表長度內
+        if(new_cell.length > 0 && puzzle.length < ADD_NEAR_RATE.length){
+            //機率表抽選是否生成
+            let add_near_rate = this.buffCtrl?.getBuffAddNearRate().slice();
+            if(Math.random() < add_near_rate[puzzle.length]){
+                //生成成功由候選隨機1個CELL加入
+                let rand:number = Math.floor(Math.random() * new_cell.length);
+                let [x, y] = new_cell[rand];
+                _puzzles[puzzle_id].push(new_cell[rand]);
+                _board[x][y] = puzzle_id; //設定棋盤上佔用
+                this.addNearCell(_puzzles, _board); //繼續生成鄰近CELL
+            }
+        }
+    }
+
+    //尋找鄰近拼圖
+    findNearPuzzle(_board:number[][], _x:number, _y:number):number
+    {
+        let direct = [[-1, 0], [1, 0], [0, 1], [0, -1]]; //4方向
+        let near:number[][] = [];
+        //尋找棋盤範圍內且已被佔用的鄰近CELL
+        for(let i = 0; i < direct.length; i++){
+            let [dx, dy] = direct[i];
+            let [x, y] = [_x + dx, _y + dy];
+            if(x >= 0 && y >= 0 && x < _board.length && y < _board[x].length && _board[x][y] > -1 && _board[x][y] != Infinity){
+                near.push([x, y]);
+            }
+        }
+        //抽選其中一個鄰近CELL的佔用拼圖ID回傳
+        if(near.length > 0){
+            let [nx, ny] = near[Math.floor(Math.random() * near.length)];
+            return _board[nx][ny];
+        }
+        return 0;
+    }
+
+    //拼圖放上棋盤事件
+    onFitPuzzle(_fit_cell:number[][], _name:string)
+    {
+        let gravity = GameModel.getGravityBuff();
+        //找方塊最高和最低y座標
+        let [min_y, max_y] = [Infinity, -1];
+        for(let i = 0; i < _fit_cell.length; i++){
+            if(_fit_cell[i][1] < min_y){
+                min_y = _fit_cell[i][1];
+            }
+            if(_fit_cell[i][1] > max_y){
+                max_y = _fit_cell[i][1];
+            }
+        }
+        let board:number[][] = GameModel.getBoard().map(v => v.slice());  //複製棋盤資訊
+        let dy:number|null = null;
+        let tar_y = 0;
+        if(gravity > 0){
+            tar_y = min_y - 0;
+        }
+        else if(gravity < 0){
+            tar_y = board.length - 1 - max_y;
+        }
+        for(let i = 0; i <= tar_y; i++){
+            let fit_cnt = 0;
+            for(let j = 0; j < _fit_cell.length; j++){
+                let [x, y] = _fit_cell[j];
+                if(board[x][y - i * gravity] == -1){
+                    fit_cnt++;
+                }
+                else{
+                    break;
+                }
+            }
+            if(fit_cnt == _fit_cell.length){
+                dy = i * gravity;
+            }
+            else{
+                break;
+            }
+        }
+        //沒有可以放上的棋盤格, 回傳結果不更新棋盤資訊
+        if(dy == null){
+            this.puzzleCtrl?.onFitPuzzleCallback(dy);
+            return;
+        }
+        let puzzle_id = parseInt(_name);  //傳入拼圖id字串改為數字
+        //放上拼圖成功回傳結果, 更新棋盤資訊
+        for(let i = 0; i < _fit_cell.length; i++){
+            let [x, y] = _fit_cell[i];
+            board[x][y - dy] = puzzle_id;
+        }
+        GameModel.setBoard(board);
+        this.puzzleCtrl?.onFitPuzzleCallback(dy);
+        this.checkResult(board); //勝利判定
+    }
+
+    //拼圖移出棋盤事件
+    onRemovePuzzle(_fit_cell:number[][], _name:string)
+    {
+        let puzzle_id = parseInt(_name); //傳入拼圖id字串改為數字
+        let board:number[][] = GameModel.getBoard().map(v => v.slice()); //複製棋盤資訊暫時修改(結果判定為失敗就不更新)
+        //判定每個CELL的棋盤資訊都是拼圖id, 填入-1
+        for(let i = 0; i < _fit_cell.length; i++){
+            let [x, y] = _fit_cell[i];
+            if(board[x][y] == puzzle_id){
+                board[x][y] = -1;
+            }
+            //遇到非拼圖格, 不更新棋盤資訊
+            else{
+                return;
+            }
+        }
+        GameModel.setBoard(board); //移出拼圖成功, 更新棋盤資訊
+    }
+
+    onStartButtonClicked()
+    {
+        this.restart();
+    }
+
+    //勝利判定
+    checkResult(_board:number[][])
+    {
+        if(GameModel.getHelpMode()){
+            return;
+        }
+        //棋盤資訊填滿為勝利
+        for(let i = 0; i < _board.length; i++){
+            for(let j = 0; j < _board[i].length; j++){
+                if(_board[i][j] == -1){
+                    return;
+                }
+            }
+        }
+        this.pause();
+    }
+
+    setMenuVisible(_pause: boolean)
+    {
+        if(this.startMenu){
+            this.startMenu.active = _pause;
+        }
+        if(this.shareBtn){
+            this.shareBtn.active = !_pause;
+        }
+        if(this.shareMenu){
+            this.shareMenu.active = false;
+        }
+    }
+
+    checkHelpMode()
+    {
+        if (typeof window !== 'undefined') {
+            let p = new URLSearchParams(window.location.search); //url query string
+            if(p.has('b') && p.has('r') && p.has('g') && p.has('o') && p.has('p')){
+                let board_len = parseInt(p.get('b'));
+                if(isNaN(board_len) || board_len < MIN_BOARD_LENGTH || board_len > MAX_BOARD_LENGTH){
+                    return;
+                }
+                let rotate = parseInt(p.get('r'));
+                if(isNaN(rotate) || rotate < 0 || rotate > 1){
+                    return;
+                }
+                let gravity = parseInt(p.get('g'));
+                if(isNaN(gravity) || gravity < -1 || gravity > 1){
+                    return;
+                }
+                let obstacles_str = p.get('o');
+                if(!/^\d+$/.test(obstacles_str) || (obstacles_str.length % 2 != 0 && obstacles_str != '0')){
+                    return;
+                }
+                let puzzles_strs = p.getAll('p');
+                for(let i = 0; i < puzzles_strs.length; i++){
+                    let puzzles_str = puzzles_strs[i];
+                    if(!/^\d+$/.test(puzzles_str) || puzzles_str.length % 2 != 0){
+                        return;
+                    }
+                }
+                GameModel.setBoardLength(board_len);
+                GameModel.setCannotRotateBuff(rotate == 0 ? true : false);
+                GameModel.setGravityBuff(gravity);
+                this.setMenuVisible(false);
+                if(this.stageLabel){
+                    this.stageLabel.string = '';
+                }
+                if(this.screenshotBtn){
+                    this.screenshotBtn.active = true;
+                }
+                let obstacle_set:Set<number> = new Set<number>()
+                if(obstacles_str != '0'){
+                    for(let i = 0; i < obstacles_str.length; i += 2){
+                        let [x, y] = [parseInt(obstacles_str[i]), parseInt(obstacles_str[i + 1])];
+                        obstacle_set.add(x * board_len + y);
+                    }
+                }
+                this.initBoard(obstacle_set);
+                let puzzles:number[][][] = [];
+                for(let i = 0; i < puzzles_strs.length; i++){
+                    let puzzles_str = puzzles_strs[i];
+                    let puzzle:number[][] = [];
+                    for(let j = 0; j < puzzles_str.length; j += 2){
+                        let [x, y] = [parseInt(puzzles_str[j]), parseInt(puzzles_str[j + 1])];
+                        puzzle.push([x, y]);
+                    }
+                    puzzles.push(puzzle);
+                }
+                GameModel.setPuzzles(puzzles);
+                this.puzzleCtrl?.initPuzzle();
+                this.puzzleCtrl?.setEventsActive(true);
+                this.setEventsActive(true);
+                GameModel.setHelpMode(true);
+            }
+        }
+    }
+
+    onShareBtnClick()
+    {
+        if(this.shareMenu){
+            if(this.shareMenu.active == false){
+                this.shareMenu.active = true;
+                if(this.shareEdit){
+                    let query_str = 'https://youthtion.github.io/cocos_web/puzzle2?';
+                    let board = GameModel.getBoard().map(v => v.slice());
+                    query_str += 'b=' + String(board.length);
+                    query_str += '&r=' + String(GameModel.getCannotRotateBuff() == true ? 0 : 1);
+                    query_str += '&g=' + String(GameModel.getGravityBuff());
+                    let o = '';
+                    for(let i = 0; i < board.length; i++){
+                        for(let j = 0; j < board[i].length; j++){
+                            if(board[i][j] == Infinity){
+                                o += String(i) + String(j);
+                            }
+                        }
+                    }
+                    if(o == ''){
+                        query_str += '&o=0';
+                    }
+                    else{
+                        query_str += '&o=' + o;
+                    }
+                    let puzzles = GameModel.getPuzzles().map(v0 => v0.map(v1 => v1.slice()));
+                    for(let i = 0; i < puzzles.length; i++){
+                        query_str += '&p=';
+                        for(let j = 0; j < puzzles[i].length; j++){
+                            let [x, y] = puzzles[i][j];
+                            query_str += String(x) + String(y);
+                        }
+                    }
+                    this.shareEdit.string = query_str;
+                }
+            }
+            else{
+                this.shareMenu.active = false;
+            }
+        }
+    }
+
+    async onCopyBtnClick()
+    {
+        if(this.shareEdit){
+            await navigator.clipboard.writeText(this.shareEdit.string);
+        }
+    }
+}
+
+
